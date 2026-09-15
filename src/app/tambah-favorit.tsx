43df081080116d877f69bde6720406cc67df2f25
Moch. Nazril Ilham <moch.nazril.ilham24071@mhs.uingusdur.ac.id>
2026-09-15 20:30:08 +0700

    feat: perbaiki modal tambah favorit

--- src/app/tambah-favorit.tsx
+++ src/app/tambah-favorit.tsx
@@ -3,13 +3,24 @@ import { router } from "expo-router";
 
 export default function ModalTambahFavorit() {
   return (
-    <View style={{ padding: 16 }}>
+    <View
+      style={{
+        flex: 1,
+        padding: 16,
+        gap: 16,
+      }}
+    >
       <Text>
         Tambahkan kota ini ke daftar favorit?
       </Text>
 
       <Button
         title="Simpan"
+        onPress={() => router.back()}
+      />
+
+      <Button
+        title="Batal"
         onPress={() => router.back()}
       />
     </View>
