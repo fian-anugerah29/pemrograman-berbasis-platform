062d128b083dadd34f85713f7a03f0b684d1281b
Moch. Nazril Ilham <moch.nazril.ilham24071@mhs.uingusdur.ac.id>
2026-09-15 20:32:54 +0700

    feat: perbaiki modal tambah favorit

--- src/app/detail/[kota].tsx
+++ src/app/detail/[kota].tsx
@@ -1,5 +1,5 @@
-import { View } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { View, Button } from "react-native";
+import { useLocalSearchParams, router } from "expo-router";
 
 import WeatherCard from "../../../components/WeatherCard";
 
@@ -7,11 +7,27 @@ export default function HalamanDetail() {
   const { kota } = useLocalSearchParams<{ kota: string }>();
 
   return (
-    <View style={{ padding: 16 }}>
+    <View
+      style={{
+        flex: 1,
+        padding: 16,
+        gap: 16,
+      }}
+    >
       <WeatherCard
         kota={kota}
         suhu={29}
         tingkatAQI="BAIK"
+      />
+
+      <Button
+        title="Tambah ke Favorit"
+        onPress={() =>
+          router.push({
+            pathname: "/tambah-favorit",
+            params: { kota },
+          })
+        }
       />
     </View>
   );
